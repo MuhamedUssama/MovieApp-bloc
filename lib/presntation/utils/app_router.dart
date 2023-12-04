e0b561cae338ff8ac65f0ec46321ce17a5115656
Mohamed <mohamed.osama200538@gmail.com>
2023-12-04 21:13:00 +0200

    add bloc provider to app router file

--- lib/presntation/utils/app_router.dart
+++ lib/presntation/utils/app_router.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/business_logic/popular/popular_movies_cubit.dart';
+import 'package:movie_app/data/api/home_screen_apis.dart';
+import 'package:movie_app/data/repository/popular_repo.dart';
 import 'package:movie_app/presntation/screens/home/home.dart';
 import 'package:movie_app/presntation/screens/splash/splash.dart';
 
 class AppRouter {
+  late PopualarRepository popualarRepository;
+  late PopularMoviesCubit popularMoviesCubit;
+
+  AppRouter() {
+    popualarRepository = PopualarRepository(HomeScreenAPIs());
+    popularMoviesCubit = PopularMoviesCubit(popualarRepository);
+  }
+
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(builder: (_) => const SplashScreen());
+
       case 'home':
-        return MaterialPageRoute(builder: (_) => HomeScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (BuildContext context) => popularMoviesCubit,
+            child: HomeScreen(),
+          ),
+        );
     }
   }
 }
